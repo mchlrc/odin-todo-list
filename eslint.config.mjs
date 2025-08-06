5ff8b6a2974c2f8c386b03f916de0f5e4a1fc1d1
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-08-05 18:01:00 -0700

    Remove eslint-config-prettier
    This might be needed later but for now we removed it.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
